c8e3da742e4e386aea5073e24e6b43a5e30dd6ff
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-03-11 00:37:31 +0600

    Create readme.m

---
+++ Assignments/Python (CSE-1112)/Task- 1/readme.m
@@ -0,0 +1,9 @@
+<h1 align="center">CSE-1102</h1>
+<h2>Question</h2>
+Take three variables and print their values. The variables will be used for showing your name,
+id, and course code. Follow Camel Case. Comment your name, ID, and date at the beginning of
+the code.
+<h3>Output should be shown in the following format:</h3>
+My name is your name
+My id is your id
+Course code - your course code
