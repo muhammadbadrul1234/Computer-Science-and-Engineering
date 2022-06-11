856d000aa2b76ca60bb928e5ab6a092d6332f07e
Badrul <badrulalomtawsyat@gmail.com>
2022-06-12 01:11:29 +0600

    first commit

--- University/Fortran/Lab-2/Labfinal-1.m
+++ University/Fortran/Lab-2/Labfinal-1.m
@@ -5,30 +5,30 @@ close all;
 
 x = input("Please enter the function :", "s");
 f = inline(x);
-a = input("Enter the lower limit a = ");
 b = input("Enter the upper limit b = ");
-n = input("Enter the number of intervals n = ");
+a = input("Enter the lower limit a = ");
+n = input("Enter the number of iterations n = ");
 h = (b-a)/n;
-i1 = 0.0;
+ii = 0;
 
 % 022LB
 % 200UA
 %6121RD
 
 for i=a+h:h:b-h
-    i1 = i1 + 2*f(i); 
+    ii = ii + 2*f(i); 
 end
 
-I = (h/2)*(f(a)+f(b)+i1);
-fprintf("The evaluated integral of the function, %f",I);
+yi = (h/2)*(f(a)+f(b)+ii);
+fprintf("The evaluated integral of the function, %f",yi);
 
 %input=1
 % Please enter the function =((1+2*(tan(x))^2))^(1/2)
-% Enter the lower limit a = 1
 % Enter the upper limit b = pi/4
+% Enter the lower limit a = 1
 % Enter the number of intervals n = 100
 %input=2
-% Please enter the function =0.2+25*x-200*(x^2)+675*(x^3)-900*(x^4)+400*(x^5)
-% Enter the lower limit a = 1
+% Please enter the function =0.2+25*x-200*(x.^2)+675*(x.^3)-900*(x.^4)+400*(x.^5)
 % Enter the upper limit b = pi/4
+% Enter the lower limit a = 1
 % Enter the number of intervals n = 50
--- University/Fortran/Lab-2/Labfinal-2.m
+++ University/Fortran/Lab-2/Labfinal-2.m
@@ -4,9 +4,9 @@ clear all;
 close all;
 
 a = input('Enter Function with right side zero: ','s');
-y = inline(a);
-a = input('Lower Limit: ');
+f = inline(a);
 b = input('Upper Limit: ');
+a = input('Lower Limit: ');
 n = input('Number of Iteration: ');
 h = (b-a)/n;
 iEven = 0;
@@ -17,13 +17,13 @@ iOdd = 0;
 %6121RD
 
 for i = a+h:2*h:b-h
-  iOdd = iOdd+y(i);
+  iOdd = iOdd+f(i);
 end
 for i = a+2*h:2*h:b-2*h
-  iEven = iEven+y(i);
+  iEven = iEven+f(i);
 end
 
-area = (h/3)*((y(a)+y(b))+4*iOdd+2*iEven);
+area = (h/3)*((f(a)+f(b))+2*iOdd+4*iEven);
 fprintf('Integrated value = %f',area);
 
 %input=1
