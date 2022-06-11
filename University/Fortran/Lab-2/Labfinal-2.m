6332ab40d2b8015652ab257cf7b28340e4fe70c8
Badrul <badrulalomtawsyat@gmail.com>
2022-06-12 01:56:15 +0600

    first commit

--- University/Fortran/Lab-2/Labfinal-2.m
+++ University/Fortran/Lab-2/Labfinal-2.m
@@ -2,7 +2,6 @@
 clc;
 clear all;
 close all;
-
 fx = input('Enter Function with right side zero: ','s');
 f = inline(fx);
 b = input('Upper Limit: ');
@@ -11,30 +10,19 @@ n = input('Number of Iteration: ');
 h = (b-a)/n;
 iEven = 0;
 iOdd = 0;
-
 % 022LB
 % 200UA
 %6121RD
-
 for x = a+h:2*h:b-h
   iOdd = iOdd+f(x);
 end
-
 for x = a+2*h:2*h:b-2*h
   iEven = iEven+f(x);
 end
-
 area = (h/3)*(f(a) + (2*iEven) + (4*iOdd) + f(b) );
 fprintf('Integrated value = %f',area);
-
-%input=1
-% Please enter the function =x.^1/2
-% Enter the lower limit a = 0
-% Enter the upper limit b = 8
-% Enter the number of intervals n = 50
-
-%input=2
-% Please enter the function = exp(x)
-% Enter the lower limit a = 0
-% Enter the upper limit b = 1
-% Enter the number of intervals n = 29
+% input=1                                                   input=2
+% Please enter the function =x.^1/2                         Please enter the function = exp(x)
+% Enter the lower limit a = 0                               Enter the lower limit a = 0
+% Enter the upper limit b = 8                               Enter the upper limit b = 1
+% Enter the number of intervals n = 50                      Enter the number of intervals n = 100
