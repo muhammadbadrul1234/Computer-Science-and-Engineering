4d385b1b699b95d9c6f10c10f7a2ac1cbc767075
Badrul <badrulalomtawsyat@gmail.com>
2022-06-12 00:31:59 +0600

    fisrt commit

---
+++ University/Fortran/Lab-2/1.m
@@ -0,0 +1,15 @@
+x = input("Please enter the function :", "s");
+f = inline(x);
+a = input("Enter the lower limit a = ");
+b = input("Enter the upper limit b = ");
+n = input("Enter the number of intervals n = ");
+%2012020216
+h = (b-a)/n;
+i1 = 0.0;
+
+for i=a+h:h:b-h
+    i1 = i1 + 2*f(i); 
+end
+I = (h/2)*(f(a)+f(b)+3*i1);
+
+fprintf("The evaluated integral of the function, %f",I);
---
+++ University/Fortran/Lab-2/3.m
@@ -0,0 +1,32 @@
+clc;
+clear all;
+close all;
+
+a = input('Enter Function: ','s');
+y = inline(a);
+
+a = input('Lower limit: ');
+b = input('Upper limit: ');
+n = input('Number of iteration: ');
+
+h = (b-a)/n;
+
+i1 = 0;
+i2 = 0;
+i3 = 0;
+
+for i=a+h:3*h:b-2*h
+  i1=i1+y(i);
+end
+
+for i=a+2*h:3*h:b-h
+  i2=i2+y(i);
+end
+
+for i=a+3*h:3*h:b-3*h
+  i3=i3+y(i);
+end
+
+area = (3*h/8)*((y(a)+y(b))+3*i1+3*i2+2*i3);
+
+fprintf('Integrated value = %f',area);
