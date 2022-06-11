4dccba19e03816c3cc65f3c95b5dd932f5fc0dca
Badrul <badrulalomtawsyat@gmail.com>
2022-06-12 00:41:15 +0600

    4

--- University/Fortran/Lab-2/1.m
+++ University/Fortran/Lab-2/1.m
@@ -1,16 +1,26 @@
 %Trapezoidal Rule
+clc;
+clear all;
+close all;
+
 x = input("Please enter the function :", "s");
 f = inline(x);
 a = input("Enter the lower limit a = ");
 b = input("Enter the upper limit b = ");
 n = input("Enter the number of intervals n = ");
-%2012020216
 h = (b-a)/n;
 i1 = 0.0;
+%2012020216
 
 for i=a+h:h:b-h
     i1 = i1 + 2*f(i); 
 end
-I = (h/2)*(f(a)+f(b)+3*i1);
 
+I = (h/2)*(f(a)+f(b)+3*i1);
 fprintf("The evaluated integral of the function, %f",I);
+
+%input
+% Please enter the function =((1+(tan(x))^2))^(1/2)
+% Enter the lower limit a = 1
+% Enter the upper limit b = pi/4
+% Enter the number of intervals n = 100
--- University/Fortran/Lab-2/2.m
+++ University/Fortran/Lab-2/2.m
@@ -5,24 +5,20 @@ close all;
 
 a = input('Enter Function with right side zero: ','s');
 y = inline(a);
-
 a = input('Lower Limit: ');
 b = input('Upper Limit: ');
 n = input('Number of Iteration: ');
-
 h = (b-a)/n;
-
 iEven = 0;
 iOdd = 0;
+
 %2012020216
 for i = a+h:2*h:b-h
   iOdd = iOdd+y(i);
 end
-
 for i = a+2*h:2*h:b-2*h
   iEven = iEven+y(i);
 end
 
 area = (h/3)*((y(a)+y(b))+4*iOdd+2*iEven);
-
 fprintf('Integrated value = %f',area);
