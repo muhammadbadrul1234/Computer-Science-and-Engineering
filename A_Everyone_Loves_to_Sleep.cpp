67c7396a664bc14f0667aca3ba7a5a2c673f4219
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-16 23:44:59 +0600

    Commit by Muhammad Badrul

--- A_Everyone_Loves_to_Sleep.cpp
+++ A_Everyone_Loves_to_Sleep.cpp
@@ -20,9 +20,7 @@ bool isPrime(int n){
     if(n==1) return false;
     if(n==2) return true;
     for(int i=2;i*i<=n;i++){
-        if(n%i==0)return false;drghedrtghertgydhertgh
-
-        4541245
+        if(n%i==0)return false;
     }return true;
 }
     /* Muhammad Badrul Alom Tawsyat
